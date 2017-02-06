a654a3e2570c7f62eb12235c383e3358fc824b52
Conrad Ratschan <ratschanc@msoe.edu>
2017-02-05 20:54:50 -0600

    Added sphere and colored sphere based on surface normals

--- src/main.rs
+++ src/main.rs
@@ -31,9 +31,28 @@ fn main() {
     }
 }
 
-pub fn color(r: &ray::Ray) -> Vec3 {
+fn color(r: &ray::Ray) -> Vec3 {
+    let t = hit_sphere(&Vec3 { x: 0.0, y: 0.0, z: -1.0 }, 0.5, r);
+    if t > 0.0 {
+        let n = vec3::unit_vector(r.point_at_parameter(t) 
+                                    - Vec3 { x: 0.0, y: 0.0, z: -1.0 });
+        return 0.5 * Vec3 { x: n.x + 1.0, y: n.y + 1.0, z: n.z + 1.0 };
+    }
     let unit_direction = vec3::unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y + 1.0);
     (1.0 - t) * Vec3 { x: 1.0, y: 1.0, z: 1.0 } +
         t * Vec3 { x: 0.5, y: 0.7, z: 1.0 }
+}
+
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - *center;
+    let a = vec3::dot(&r.direction(), &r.direction());
+    let b = 2.0 * vec3::dot(&oc, &r.direction());
+    let c = vec3::dot(&oc, &oc) - radius*radius;
+    let descriminant = b*b - 4.0*a*c;
+    if descriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - descriminant.sqrt()) / (2.0 * a)
+    }
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Neg, Mul, Div};
+use std::ops::{Add, Neg, Mul, Div, Sub};
 
 #[derive(Copy, Clone)]
 pub struct Vec3 {
@@ -36,6 +36,16 @@ impl Add for Vec3 {
         Vec3{ x: self.x + other.x,
                 y: self.y + other.y,
                 z: self.z + other.z }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3{ x: self.x - other.x,
+                y: self.y - other.y,
+                z: self.z - other.z }
     }
 }
 
